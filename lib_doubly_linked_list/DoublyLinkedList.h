0953e9877fc48ef9c71244f7ff38a807e40ba722
Артём Денисов <temadenwin@mail.ru>
2025-11-04 14:53:36 +0300

    Implemented the DoublyLinkedList class and covered all methods with tests.

--- lib_doubly_linked_list/DoublyLinkedList.h
+++ lib_doubly_linked_list/DoublyLinkedList.h
@@ -8,26 +8,25 @@
 namespace doubly_linked {
 	template<class T> struct Node {
 		T value;
+		Node* prev;
 		Node* next;
-		Node* previous;
 
-		Node(T _value, Node* _next, Node* _previous) : value(_value), next(_next), previous(_previous) {};
+		Node(T _value, Node* _prev, Node* _next) : value(_value), prev(_prev), next(_next) {};
 	};
 }
 
 template<class T> class DoublyLinkedList {
-/*
 	doubly_linked::Node<T>* _head;
 	doubly_linked::Node<T>* _tail;
 	size_t _size;
 public:
 	// Constructors
-	List();
-	List(size_t);
-	List(size_t, const T&);
-	List(const TVector<T>&);
-	List(std::initializer_list<T>);
-	List(const List&);
+	DoublyLinkedList();
+	DoublyLinkedList(size_t);
+	DoublyLinkedList(size_t, const T&);
+	DoublyLinkedList(const TVector<T>&);
+	DoublyLinkedList(std::initializer_list<T>);
+	DoublyLinkedList(const DoublyLinkedList&);
 
 	// Destructor
 	~DoublyLinkedList();
@@ -61,5 +60,495 @@ public:
 	DoublyLinkedList& operator=(const TVector<T>&);
 	bool operator==(const DoublyLinkedList&) const;
 	bool operator!=(const DoublyLinkedList&) const;
-	*/
+
+	// Class Iterator
+	class Iterator {
+		doubly_linked::Node<T>* _current;
+	public:
+		Iterator() : _current(nullptr) {};
+		Iterator(doubly_linked::Node<T>* node) : _current(node) {};
+
+		Iterator& operator=(const Iterator& other) {
+			if (this != &other) {
+				_current = other._current;
+			}
+			return *this;
+		}
+
+		Iterator& operator++() {
+			if (_current != nullptr) {
+				_current = _current->next;
+			}
+			return *this;
+		}
+		Iterator operator++(int) {
+			Iterator iter = *this;
+			++(*this);
+			return iter;
+		}
+
+		Iterator& operator--() {
+			if (_current != nullptr) {
+				_current = _current->prev;
+			}
+			return *this;
+		}
+		Iterator operator--(int) {
+			Iterator iter = *this;
+			--(*this);
+			return iter;
+		}
+
+		bool operator==(const Iterator& other) const {
+			return _current == other._current;
+		}
+
+		bool operator!=(const Iterator& other) const {
+			return _current != other._current;
+		}
+
+		T& operator*() {
+			if (_current == nullptr) {
+				throw std::logic_error("DoublyLinkedList::Iterator.operator*: Current node is nullptr");
+			}
+			return _current->value;
+		}
+
+		Iterator& operator+=(size_t gap) {
+			for (size_t i = 0; i < gap; i++) {
+				if (_current == nullptr) break;
+				_current = _current->next;
+			}
+			return *this;
+		}
+
+		Iterator& operator-=(size_t gap) {
+			for (size_t i = 0; i < gap; i++) {
+				if (_current == nullptr) break;
+				_current = _current->prev;
+			}
+			return *this;
+		}
+	};
+
+	// Class ConstIterator
+	class ConstIterator {
+		const doubly_linked::Node<T>* _current;
+	public:
+		ConstIterator() : _current(nullptr) {};
+		ConstIterator(const doubly_linked::Node<T>* node) : _current(node) {};
+
+		ConstIterator& operator=(const ConstIterator& other) {
+			if (this != &other) {
+				_current = other._current;
+			}
+			return *this;
+		}
+
+		ConstIterator& operator++() {
+			if (_current != nullptr) {
+				_current = _current->next;
+			}
+			return *this;
+		}
+
+		ConstIterator operator--(int) {
+			ConstIterator iter = *this;
+			--(*this);
+			return iter;
+		}
+
+		ConstIterator& operator--() {
+			if (_current != nullptr) {
+				_current = _current->prev;
+			}
+			return *this;
+		}
+
+		ConstIterator operator++(int) {
+			ConstIterator iter = *this;
+			--(*this);
+			return iter;
+		}
+
+		bool operator==(const ConstIterator& other) const {
+			return _current == other._current;
+		}
+
+		bool operator!=(const ConstIterator& other) const {
+			return _current != other._current;
+		}
+
+		const T& operator*() const {
+			if (_current == nullptr) {
+				throw std::logic_error("List::ConstIterator.operator*: Current node is nullptr");
+			}
+			return _current->value;
+		}
+
+		ConstIterator& operator+=(size_t gap) {
+			for (size_t i = 0; i < gap; i++) {
+				if (_current == nullptr) break;
+				_current = _current->next;
+			}
+			return *this;
+		}
+
+		ConstIterator& operator-=(size_t gap) {
+			for (size_t i = 0; i < gap; i++) {
+				if (_current == nullptr) break;
+				_current = _current->prev;
+			}
+			return *this;
+		}
+	};
+
+	// Getters associated with the Iterator class
+	inline Iterator begin() { return Iterator(_head); };
+	inline Iterator end() { return Iterator(nullptr); };
+	inline ConstIterator begin() const { return ConstIterator(_head); };
+	inline ConstIterator end() const { return ConstIterator(nullptr); };
 };
+
+// Constructors
+
+template<class T> DoublyLinkedList<T>::DoublyLinkedList() :
+	_head(nullptr),
+	_tail(nullptr),
+	_size(0)
+{}
+
+template<class T> DoublyLinkedList<T>::DoublyLinkedList(size_t size) :
+	_head(nullptr),
+	_tail(nullptr),
+	_size(0)
+{
+	for (size_t i = 0; i < size; i++) {
+		pushBack(T());
+	}
+}
+
+template<class T> DoublyLinkedList<T>::DoublyLinkedList(size_t size, const T& value) :
+	_head(nullptr),
+	_tail(nullptr),
+	_size(0)
+{
+	for (size_t i = 0; i < size; i++) {
+		pushBack(value);
+	}
+}
+
+template<class T> DoublyLinkedList<T>::DoublyLinkedList(const TVector<T>& vector) :
+	_head(nullptr),
+	_tail(nullptr),
+	_size(0)
+{
+	if (!vector.is_empty()) {
+		for (size_t i = 0; i < vector.size(); i++) {
+			pushBack(vector[i]);
+		}
+	}
+}
+
+template<class T> DoublyLinkedList<T>::DoublyLinkedList(const std::initializer_list<T> init) :
+	_head(nullptr),
+	_tail(nullptr),
+	_size(0)
+{
+	for (const T& elem : init) {
+		pushBack(elem);
+	}
+}
+
+template<class T> DoublyLinkedList<T>::DoublyLinkedList(const DoublyLinkedList& other) :
+	_head(nullptr),
+	_tail(nullptr),
+	_size(0)
+{
+	doubly_linked::Node<T>* current = other._head;
+	while (current != nullptr) {
+		pushBack(current->value);
+		current = current->next;
+	}
+}
+
+// Destructor
+
+template<class T> DoublyLinkedList<T>::~DoublyLinkedList() {
+	clear();
+}
+
+// Functions
+
+template<class T> DoublyLinkedList<T>& DoublyLinkedList<T>::assign(const DoublyLinkedList& other) {
+	if (this != &other) {
+		clear();
+		_size = 0;
+		doubly_linked::Node<T>* current = other._head;
+		while (current != nullptr) {
+			pushBack(current->value);
+			current = current->next;
+		}
+	}
+	return *this;
+}
+
+template<class T> DoublyLinkedList<T>& DoublyLinkedList<T>::assign(const TVector<T>& vector) {
+	if (vector.size() == 0) {
+		throw std::logic_error("DoublyLinkedList.assign_TVector: 'vector.size' must be > 0");
+	}
+
+	clear();
+	for (size_t i = 0; i < vector.size(); i++) {
+		pushBack(vector[i]);
+	}
+
+	return *this;
+}
+
+template<class T> bool DoublyLinkedList<T>::isEqual(const DoublyLinkedList& other) const {
+	if (_size != other._size) return false;
+
+	const doubly_linked::Node<T>* current1 = _head;
+	const doubly_linked::Node<T>* current2 = other._head;
+
+	while (current1 != nullptr && current2 != nullptr) {
+		if (current1->value != current2->value) return false;
+		current1 = current1->next;
+		current2 = current2->next;
+	}
+	return true;
+}
+
+template<class T> TVector<T> DoublyLinkedList<T>::toTVector() const {
+	TVector<T> result;
+	doubly_linked::Node<T>* current = _head;
+	while (current != nullptr) {
+		result.push_back(current->value);
+		current = current->next;
+	}
+
+	return result;
+}
+
+template<class T> void DoublyLinkedList<T>::pushFront(const T& value) {
+	_size++;
+	doubly_linked::Node<T>* node = new doubly_linked::Node<T>(value, nullptr, _head);
+
+	if (_head != nullptr) {
+		_head->prev = node;
+	}
+
+	if (isEmpty()) {
+		_tail = node;
+	}
+	_head = node;
+}
+
+template<class T> void DoublyLinkedList<T>::pushBack(const T& value) {
+	_size++;
+	doubly_linked::Node<T>* node = new doubly_linked::Node<T>(value, _tail, nullptr);
+
+	if (isEmpty()) {
+		_head = node;
+		_tail = node;
+	}
+	else {
+		_tail->next = node;
+		_tail = node;
+	}
+}
+
+template<class T> void DoublyLinkedList<T>::insert(size_t pos, const T& value) {
+	if (pos > _size) {
+		throw std::out_of_range("DoublyLinkedList.insert: 'pos' out of range");
+	}
+
+	if (pos == 0) {
+		pushFront(value);
+	}
+	else if (pos == _size) {
+		pushBack(value);
+	}
+	else {
+		doubly_linked::Node<T>* current = _head;
+		size_t currentPos = 0;
+		while (current != nullptr) {
+			if (currentPos == pos - 1) {
+				break;
+			}
+			currentPos++;
+			current = current->next;
+		}
+		insertAtNode(current, value);
+	}
+}
+
+template<class T> void DoublyLinkedList<T>::insertAtNode(doubly_linked::Node<T>* pos, const T& value) {
+	if (pos == nullptr) {
+		throw std::logic_error("DoublyLinkedList.insert: Node pointer is null");
+	}
+
+	if (isEmpty()) {
+		throw std::logic_error("DoublyLinkedList.insert: List is empty");
+	}
+
+	doubly_linked::Node<T>* node = new doubly_linked::Node<T>(value, pos, pos->next);
+	pos->next = node;
+	if (node->next != nullptr) {
+		node->next->prev = node;
+	}
+	if (pos == _tail) {
+		_tail = node;
+	}
+	_size++;
+}
+
+template<class T> void DoublyLinkedList<T>::popFront() {
+	if (isEmpty()) {
+		throw std::logic_error("DoublyLinkedList.popFront: List is empty");
+	}
+
+	doubly_linked::Node<T>* node = _head;
+	_head = _head->next;
+
+	if (_head != nullptr) {
+		_head->prev = nullptr;
+	}
+	else {
+		_tail = nullptr;
+	}
+
+	delete node;
+	_size--;
+}
+
+template<class T> void DoublyLinkedList<T>::popBack() {
+	if (isEmpty()) {
+		throw std::logic_error("DoublyLinkedList.popBack: List is empty");
+	}
+
+	if (_head == _tail) {
+		delete _head;
+		_head = nullptr;
+		_tail = nullptr;
+	}
+	else {
+		doubly_linked::Node<T>* newTail = _tail->prev;
+		newTail->next = nullptr;
+		delete _tail;
+		_tail = newTail;
+	}
+	_size--;
+}
+
+template<class T> void DoublyLinkedList<T>::erase(size_t pos) {
+	if (pos >= _size) {
+		throw std::out_of_range("DoublyLinkedList.insert: 'pos' out of range");
+	}
+
+	if (isEmpty()) {
+		throw std::logic_error("DoublyLinkedList.erase: List is empty");
+	}
+
+	if (pos == 0) {
+		popFront();
+	}
+	else if (pos == _size - 1) {
+		popBack();
+	}
+	else {
+		doubly_linked::Node<T>* current = _head;
+		size_t currentPos = 0;
+		while (current != nullptr && currentPos < pos) {
+			currentPos++;
+			current = current->next;
+		}
+
+		current->prev->next = current->next;
+		if (current->next != nullptr) {
+			current->next->prev = current->prev;
+		}
+		delete current;
+		_size--;
+	}
+}
+
+template<class T> void DoublyLinkedList<T>::eraseNode(doubly_linked::Node<T>* pos) {
+	if (pos == nullptr) {
+		throw std::logic_error("DoublyLinkedList.erase: Node pointer is null");
+	}
+
+	if (isEmpty()) {
+		throw std::logic_error("DoublyLinkedList.erase: List is empty");
+	}
+
+	if (pos == _head) {
+		popFront();
+	}
+	else if (pos == _tail) {
+		popBack();
+	}
+	else {
+		pos->prev->next = pos->next;
+		pos->next->prev = pos->prev;
+		delete pos;
+		_size--;
+	}
+}
+
+template<class T> void DoublyLinkedList<T>::clear() {
+	while (_head != nullptr) {
+		doubly_linked::Node<T>* current = _head;
+		_head = _head->next;
+		delete current;
+	}
+	_tail = nullptr;
+	_size = 0;
+}
+
+// Getters
+
+template<class T> T& DoublyLinkedList<T>::front() {
+	if (isEmpty()) {
+		throw std::logic_error("DoublyLinkedList.front: Unable to access front element - list is empty");
+	}
+	return _head->value;
+}
+template<class T> const T& DoublyLinkedList<T>::front() const {
+	if (isEmpty()) {
+		throw std::logic_error("DoublyLinkedList.front: Unable to access front element - list is empty");
+	}
+	return _head->value;
+}
+template<class T> T& DoublyLinkedList<T>::back() {
+	if (isEmpty()) {
+		throw std::logic_error("DoublyLinkedList.back: Unable to access back element - list is empty");
+	}
+	return _tail->value;
+}
+
+template<class T> const T& DoublyLinkedList<T>::back() const {
+	if (isEmpty()) {
+		throw std::logic_error("DoublyLinkedList.back: Unable to access back element - list is empty");
+	}
+	return _tail->value;
+}
+
+// Operators overload
+
+template<class T> DoublyLinkedList<T>& DoublyLinkedList<T>::operator=(const DoublyLinkedList& other) {
+	return this->assign(other);
+}
+
+template<class T> DoublyLinkedList<T>& DoublyLinkedList<T>::operator=(const TVector<T>& vector) {
+	return this->assign(vector);
+}
+
+template<class T> bool DoublyLinkedList<T>::operator==(const DoublyLinkedList& other) const {
+	return this->isEqual(other);
+}
+
+template<class T> bool DoublyLinkedList<T>::operator!=(const DoublyLinkedList& other) const {
+	return !(*this == other);
+}
---
+++ tests/test_DoublyLinkedList.cpp
@@ -0,0 +1,401 @@
+#include <gtest/gtest.h>
+
+#include "../lib_doubly_linked_list/DoublyLinkedList.h"
+#include "../lib_tvector/TVector.h"
+
+#include <string>
+
+TEST(DoublyLinkedListTest, DefaultConstructor) {
+	DoublyLinkedList<int> l;
+	EXPECT_EQ(l.isEmpty(), true);
+	EXPECT_EQ(l.size(), 0);
+	EXPECT_EQ(l.begin(), l.end());
+}
+
+TEST(DoublyLinkedListTest, SizeConstructor) {
+	DoublyLinkedList<int> l(5);
+	EXPECT_EQ(l.isEmpty(), false);
+	EXPECT_EQ(l.size(), 5);
+	TVector<int> values;
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<int>({ 0, 0, 0, 0, 0 }));
+}
+
+TEST(DoublyLinkedListTest, SizeDataConstructor) {
+	DoublyLinkedList<int> l(5, 4);
+	EXPECT_EQ(l.isEmpty(), false);
+	EXPECT_EQ(l.size(), 5);
+	TVector<int> values;
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<int>({ 4, 4, 4, 4, 4 }));
+}
+
+TEST(DoublyLinkedListTest, TVectorConstructor) {
+	TVector<int> vec({ 2, 7, 45, -7 });
+	DoublyLinkedList<int> l(vec);
+	EXPECT_EQ(l.isEmpty(), false);
+	EXPECT_EQ(l.size(), 4);
+	TVector<int> values;
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<int>({ 2, 7, 45, -7 }));
+}
+
+TEST(DoublyLinkedListTest, InitListConstructor) {
+	std::initializer_list<int> init({ 7, 77, 777, -7777, 5, 6 });
+	DoublyLinkedList<int> l(init);
+	EXPECT_EQ(l.isEmpty(), false);
+	EXPECT_EQ(l.size(), 6);
+	TVector<int> values;
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<int>({ 7, 77, 777, -7777, 5, 6 }));
+}
+
+TEST(DoublyLinkedListTest, CopyConstructor) {
+	DoublyLinkedList<int> data({ 19, 18, 17, 14, 17 });
+	DoublyLinkedList<int> l(data);
+	EXPECT_EQ(l.isEmpty(), false);
+	EXPECT_EQ(l.size(), 5);
+	TVector<int> values;
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<int>({ 19, 18, 17, 14, 17 }));
+}
+
+TEST(DoublyLinkedListTest, AssignList) {
+	DoublyLinkedList<int> data({ 19, 18, 17, 14, 17 });
+	DoublyLinkedList<int> l;
+	l.assign(data);
+	EXPECT_EQ(l.isEmpty(), false);
+	EXPECT_EQ(l.size(), 5);
+	TVector<int> values;
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<int>({ 19, 18, 17, 14, 17 }));
+
+	data.popBack();
+	data.popBack();
+	data.popBack();
+	values.clear();
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<int>({ 19, 18, 17, 14, 17 }));
+}
+
+TEST(DoublyLinkedListTest, AssignTVector) {
+	TVector<int> vec({ 1, 2, -3, 40 });
+	DoublyLinkedList<int> l;
+	l.assign(vec);
+	EXPECT_EQ(l.isEmpty(), false);
+	EXPECT_EQ(l.size(), 4);
+	TVector<int> values;
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<int>({ 1, 2, -3, 40 }));
+
+	vec.pop_back();
+	vec.pop_back();
+	values.clear();
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<int>({ 1, 2, -3, 40 }));
+}
+
+TEST(DoublyLinkedListTest, IsEqual) {
+	DoublyLinkedList<int> l1({ 3, 1 });
+	DoublyLinkedList<int> l2({ 3, 1, 5, 7 });
+	l1.pushBack(5);
+	l1.pushBack(7);
+	EXPECT_EQ(l1 == l2, true);
+}
+
+TEST(DoublyLinkedListTest, IsNotEqual) {
+	DoublyLinkedList<int> l1({ 3, 1 });
+	DoublyLinkedList<int> l2({ 3, 1, 5, 7 });
+	EXPECT_EQ(l1 != l2, true);
+}
+
+TEST(DoublyLinkedListTest, ToTVector) {
+	TVector<int> data({ 1, 2, -3, 40 });
+	DoublyLinkedList<int> l({ 1, 2, -3, 40 });
+	TVector<int> vec;
+	vec = l.toTVector();
+	EXPECT_TRUE(vec == data);
+}
+
+TEST(DoublyLinkedListTest, PushFront) {
+	DoublyLinkedList<double> l({ 3.17 });
+	l.pushFront(0.44);
+	l.pushFront(1.72);
+	l.pushFront(2.18);
+	EXPECT_EQ(l.isEmpty(), false);
+	TVector<double> values;
+	for (double val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<double>({ 2.18, 1.72, 0.44, 3.17 }));
+}
+
+TEST(DoublyLinkedListTest, PushBack) {
+	DoublyLinkedList<double> l({ 5.93 });
+	l.pushBack(0.44);
+	l.pushBack(1.72);
+	l.pushBack(2.18);
+	EXPECT_EQ(l.isEmpty(), false);
+	TVector<double> values;
+	for (double val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<double>({ 5.93, 0.44, 1.72, 2.18 }));
+}
+
+TEST(DoublyLinkedListTest, InsertOutOfRangeExeption) {
+	DoublyLinkedList<int> l({ 4, 5 });
+	ASSERT_ANY_THROW(l.insert(4, 5));
+}
+
+TEST(DoublyLinkedListTest, InsertIntoMiddlePosition) {
+	DoublyLinkedList<std::string> l({ "TEST1" });
+	l.pushBack("TEST2");
+	l.pushBack("TEST3");
+	l.pushBack("TEST4");
+	l.insert(2, "TESTINSERT");
+	EXPECT_EQ(l.isEmpty(), false);
+	TVector<std::string> values;
+	for (std::string val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<std::string>({ "TEST1", "TEST2", "TESTINSERT", "TEST3", "TEST4" }));
+}
+
+TEST(DoublyLinkedListTest, InsertAtFront) {
+	DoublyLinkedList<std::string> l({ "TEST1" });
+	l.pushBack("TEST2");
+	l.pushBack("TEST3");
+	l.pushBack("TEST4");
+	l.insert(0, "TESTINSERT");
+	EXPECT_EQ(l.isEmpty(), false);
+	TVector<std::string> values;
+	for (std::string val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<std::string>({ "TESTINSERT", "TEST1", "TEST2", "TEST3", "TEST4" }));
+}
+
+TEST(DoublyLinkedListTest, InsertAtBack) {
+	DoublyLinkedList<std::string> l({ "TEST1" });
+	l.pushBack("TEST2");
+	l.pushBack("TEST3");
+	l.pushBack("TEST4");
+	l.insert(4, "TESTINSERT");
+	EXPECT_EQ(l.isEmpty(), false);
+	TVector<std::string> values;
+	for (std::string val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<std::string>({ "TEST1", "TEST2", "TEST3", "TEST4", "TESTINSERT" }));
+}
+
+TEST(DoublyLinkedListTest, InsertNodeNullptrExeption) {
+	DoublyLinkedList<int> l({ 1, 2 });
+	ASSERT_ANY_THROW(l.insertAtNode(nullptr, 4));
+}
+
+TEST(DoublyLinkedListTest, InsertToEmptyListExeption) {
+	DoublyLinkedList<int> l;
+	ASSERT_ANY_THROW(l.insertAtNode(&doubly_linked::Node<int>(1, nullptr, nullptr), 4));
+}
+
+TEST(DoublyLinkedListTest, PopFrontEmptyListExeption) {
+	DoublyLinkedList<int> l;
+	ASSERT_ANY_THROW(l.popFront());
+}
+
+TEST(DoublyLinkedListTest, PopFront) {
+	DoublyLinkedList<int> l({ 1, 2, 3, 4, 5, 6 });
+	l.popFront();
+	l.popFront();
+	l.popFront();
+	EXPECT_EQ(l.isEmpty(), false);
+	EXPECT_EQ(l.size(), 3);
+	TVector<int> values;
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<int>({ 4, 5, 6 }));
+	l.popFront();
+	l.popFront();
+	l.popFront();
+	EXPECT_EQ(l.isEmpty(), true);
+	EXPECT_EQ(l.size(), 0);
+}
+
+TEST(DoublyLinkedListTest, PopBackEmptyListExeption) {
+	DoublyLinkedList<int> l;
+	ASSERT_ANY_THROW(l.popBack());
+}
+
+TEST(DoublyLinkedListTest, PopBack) {
+	DoublyLinkedList<int> l({ 1, 2, 3, 4, 5, 6 });
+	l.popBack();
+	l.popBack();
+	l.popBack();
+	EXPECT_EQ(l.isEmpty(), false);
+	EXPECT_EQ(l.size(), 3);
+	TVector<int> values;
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<int>({ 1, 2, 3 }));
+	l.popBack();
+	l.popBack();
+	l.popBack();
+	EXPECT_EQ(l.isEmpty(), true);
+	EXPECT_EQ(l.size(), 0);
+}
+
+TEST(DoublyLinkedListTest, EraseOutOfRangeExeption) {
+	DoublyLinkedList<int> l({ 4, 5 });
+	ASSERT_ANY_THROW(l.erase(2));
+}
+
+TEST(DoublyLinkedListTest, EraseMiddlePosition) {
+	DoublyLinkedList<std::string> l({ "TEST1" });
+	l.pushBack("TEST2");
+	l.pushBack("TEST3");
+	l.pushBack("TEST4");
+	l.erase(2);
+	l.erase(2);
+	EXPECT_EQ(l.isEmpty(), false);
+	TVector<std::string> values;
+	for (std::string val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<std::string>({ "TEST1", "TEST2" }));
+}
+
+TEST(DoublyLinkedListTest, EraseFront) {
+	DoublyLinkedList<std::string> l({ "TEST1" });
+	l.pushBack("TEST2");
+	l.pushBack("TEST3");
+	l.pushBack("TEST4");
+	l.erase(0);
+	EXPECT_EQ(l.isEmpty(), false);
+	TVector<std::string> values;
+	for (std::string val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<std::string>({ "TEST2", "TEST3", "TEST4" }));
+}
+
+TEST(DoublyLinkedListTest, EraseBack) {
+	DoublyLinkedList<std::string> l({ "TEST1" });
+	l.pushBack("TEST2");
+	l.pushBack("TEST3");
+	l.pushBack("TEST4");
+	l.erase(3);
+	l.erase(2);
+	EXPECT_EQ(l.isEmpty(), false);
+	TVector<std::string> values;
+	for (std::string val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, TVector<std::string>({ "TEST1", "TEST2" }));
+}
+
+TEST(DoublyLinkedListTest, EraseNodeNullptrExeption) {
+	DoublyLinkedList<int> l({ 1, 2 });
+	ASSERT_ANY_THROW(l.eraseNode(nullptr));
+}
+
+TEST(DoublyLinkedListTest, EraseEmptyListExeption) {
+	DoublyLinkedList<int> l;
+	ASSERT_ANY_THROW(l.eraseNode(&doubly_linked::Node<int>(1, nullptr, nullptr)));
+}
+
+TEST(DoublyLinkedListTest, IteratorEmptyList) {
+	DoublyLinkedList<int> l;
+
+	EXPECT_EQ(l.begin(), l.end());
+
+	int count = 0;
+	for (auto it = l.begin(); it != l.end(); ++it) {
+		count++;
+	}
+	EXPECT_EQ(count, 0);
+}
+
+TEST(DoublyLinkedListTest, IteratorReadOperations) {
+	DoublyLinkedList<int> l({ 1, 4, 3 });
+	l.pushBack(1);
+
+	auto it = l.begin();
+	EXPECT_EQ(*it, 1);
+	++it;
+	EXPECT_EQ(*it, 4);
+	++it;
+	EXPECT_EQ(*it, 3);
+	++it;
+	EXPECT_EQ(*it, 1);
+
+	std::vector<int> values;
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, std::vector<int>({ 1, 4, 3, 1 }));
+}
+
+TEST(DoublyLinkedListTest, IteratorWriteOperations) {
+	DoublyLinkedList<int> l({ 11, 22 });
+
+	auto it = l.begin();
+	*it = 100;
+	++it;
+	*it = 200;
+
+	it = l.begin();
+	EXPECT_EQ(*it, 100);
+	++it;
+	EXPECT_EQ(*it, 200);
+}
+
+TEST(DoublyLinkedListTest, ConstIteratorEmptyList) {
+	const DoublyLinkedList<int> l;
+
+	EXPECT_EQ(l.begin(), l.end());
+
+	int count = 0;
+	for (auto it = l.begin(); it != l.end(); ++it) {
+		count++;
+	}
+	EXPECT_EQ(count, 0);
+}
+
+TEST(DoublyLinkedListTest, ConstIteratorReadOperations) {
+	const DoublyLinkedList<int> l({ 1, 4, 3 });
+
+	auto it = l.begin();
+	EXPECT_EQ(*it, 1);
+	++it;
+	EXPECT_EQ(*it, 4);
+	++it;
+	EXPECT_EQ(*it, 3);
+
+	std::vector<int> values;
+	for (int val : l) {
+		values.push_back(val);
+	}
+	EXPECT_EQ(values, std::vector<int>({ 1, 4, 3 }));
+}
