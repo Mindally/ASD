0cc0a95a77307e50dfb01b9ec8dd2bbba43b4c1b
unknown <temadenwin@mail.ru>
2025-11-18 10:09:01 +0300

    Implemented a ListBasedQueue class and covered it with tests.

---
+++ lib_list_based_queue/ListBasedQueue.h
@@ -0,0 +1,191 @@
+#pragma once
+
+#include "../lib_TVector/TVector.h"
+#include "../lib_list/List.h"
+
+#include <stdexcept>
+#include <initializer_list>
+
+#define QUEUE_DEFAULT_SIZE 10
+
+template<class T> class ListBasedQueue {
+	List<T> _data;
+	size_t _size;
+public:
+	// Constructors
+	ListBasedQueue();
+	ListBasedQueue(size_t);
+	ListBasedQueue(size_t, const T&);
+	ListBasedQueue(const TVector<T>&);
+	ListBasedQueue(const std::initializer_list<T>);
+	ListBasedQueue(const ListBasedQueue&);
+
+	// Functions
+	ListBasedQueue& assign(const ListBasedQueue&);
+	ListBasedQueue& assign(const TVector<T>&);
+	TVector<T> toTVector() const;
+
+	void push(T val);
+	void pop();
+	T& head();
+	T& tail();
+	const T& head() const;
+	const T& tail() const;
+	void clear() noexcept;
+
+	void reserve(size_t) noexcept;
+	void shrinkToFit();
+
+	// Getters
+	inline bool isEmpty() const noexcept { return _data.isEmpty(); };
+	inline bool isFull() const noexcept { return _data.size() == _size; };
+	inline size_t size() const noexcept { return _data.size(); }
+	inline size_t capacity() const noexcept { return _size; }
+
+	// Operators overload
+	ListBasedQueue& operator=(const ListBasedQueue&);
+	ListBasedQueue<T>& operator=(const TVector<T>&);
+};
+
+// Constructors
+
+template<class T> ListBasedQueue<T>::ListBasedQueue() :
+	_size(QUEUE_DEFAULT_SIZE)
+{}
+
+template<class T> ListBasedQueue<T>::ListBasedQueue(size_t size) :
+	_size(size)
+{
+	if (size == 0) {
+		throw std::invalid_argument("ListBasedQueue.size_constructor: 'size' must be > 0");
+	}
+}
+
+template<class T> ListBasedQueue<T>::ListBasedQueue(size_t size, const T& value) :
+	_size(size)
+{
+	if (size == 0) {
+		throw std::invalid_argument("Queue.size_constructor: 'size' must be > 0");
+	}
+	for (size_t i = 0; i < _size; i++) {
+		_data.pushBack(value);
+	}
+}
+
+template<class T> ListBasedQueue<T>::ListBasedQueue(const TVector<T>& vector) :
+	_size(vector.size())
+{
+	if (vector.size() == 0) {
+		throw std::invalid_argument("ListBasedQueue.fromTVector_constructor: 'size' must be > 0");
+	}
+	for (size_t i = 0; i < _size; i++) {
+		_data.pushBack(vector[i]);
+	}
+}
+
+template<class T> ListBasedQueue<T>::ListBasedQueue(const std::initializer_list<T> init) :
+	_size(init.size())
+{
+	if (init.size() == 0) {
+		throw std::invalid_argument("ListBasedQueue.fromInitializeList_constructor: 'size' must be > 0");
+	}
+	size_t i = 0;
+	for (const auto& elem : init) {
+		_data.pushBack(elem);
+	}
+}
+
+template<class T> ListBasedQueue<T>::ListBasedQueue(const ListBasedQueue& other) :
+	_size(other._size)
+{
+	_data = other._data;
+}
+
+// Functions
+
+template<class T> ListBasedQueue<T>& ListBasedQueue<T>::assign(const ListBasedQueue& other) {
+	if (this != &other) {
+		_data.assign(other._data);
+		_size = other._size;
+	}
+	return *this;
+}
+
+template<class T> ListBasedQueue<T>& ListBasedQueue<T>::assign(const TVector<T>& vector) {
+	_data.assign(vector);
+	_size = vector.size();
+	return *this;
+}
+
+template<class T> TVector<T> ListBasedQueue<T>::toTVector() const {
+	return _data.toTVector();
+}
+
+template<class T> void ListBasedQueue<T>::push(T val) {
+	if (isFull()) {
+		throw std::logic_error("ListBasedQueue.push: Unable to push - Queue is full");
+	}
+	_data.pushBack(val);
+}
+
+template<class T> void ListBasedQueue<T>::pop() {
+	if (isEmpty()) {
+		throw std::logic_error("ListBasedQueue.pop: Unable to pop - Queue is empty");
+	}
+	_data.popFront();
+}
+
+template<class T> T& ListBasedQueue<T>::head() {
+	if (isEmpty()) {
+		throw std::logic_error("ListBasedQueue.head: Unable to get head element - Queue is empty");
+	}
+	return _data.front();
+}
+
+template<class T> T& ListBasedQueue<T>::tail() {
+	if (isEmpty()) {
+		throw std::logic_error("ListBasedQueue.top: Unable to get top element - Queue is empty");
+	}
+	return _data.back();
+}
+
+template<class T> const T& ListBasedQueue<T>::head() const {
+	if (isEmpty()) {
+		throw std::logic_error("ListBasedQueue.head: Unable to get head element - Queue is empty");
+	}
+	return _data.front();
+}
+
+template<class T> const T& ListBasedQueue<T>::tail() const {
+	if (isEmpty()) {
+		throw std::logic_error("ListBasedQueue.top: Unable to get top element - Queue is empty");
+	}
+	return _data.back();
+}
+
+template<class T> void ListBasedQueue<T>::clear() noexcept {
+	_data.clear();
+}
+
+template<class T> void ListBasedQueue<T>::reserve(size_t newCapacity) noexcept {
+	if (newCapacity <= _size) return;
+	_size = newCapacity;
+}
+
+template<class T> void ListBasedQueue<T>::shrinkToFit() {
+	if (isEmpty()) {
+		throw std::logic_error("Queue.shrinkToFit: Unable to shrink - Queue is empty");
+	}
+	if (isFull()) return;
+	_size = _data.size();
+}
+
+// Operators overload
+
+template<class T> ListBasedQueue<T>& ListBasedQueue<T>::operator=(const ListBasedQueue& other) {
+	return this->assign(other);
+}
+
+template<class T> ListBasedQueue<T>& ListBasedQueue<T>::operator=(const TVector<T>& vector) {
+	return this->assign(vector);
+}
---
+++ tests/test_ListBasedQueue.cpp
@@ -0,0 +1,276 @@
+#include <gtest/gtest.h>
+
+#include "../lib_list_based_queue/ListBasedQueue.h"
+
+TEST(ListBasedQueueTest, DefaultConstructor) {
+	ListBasedQueue<int> q;
+	EXPECT_EQ(q.isEmpty(), true);
+	EXPECT_EQ(q.isFull(), false);
+	EXPECT_EQ(q.size(), 0);
+	EXPECT_EQ(q.capacity(), QUEUE_DEFAULT_SIZE);
+	ASSERT_ANY_THROW(q.head());
+	ASSERT_ANY_THROW(q.tail());
+}
+
+TEST(ListBasedQueueTest, ZeroSizeConstructor) {
+	ASSERT_ANY_THROW(ListBasedQueue<int> s(0));
+}
+
+TEST(ListBasedQueueTest, SizeConstructor) {
+	ListBasedQueue<int> q(10);
+	EXPECT_EQ(q.isEmpty(), true);
+	EXPECT_EQ(q.isFull(), false);
+	EXPECT_EQ(q.size(), 0);
+	EXPECT_EQ(q.capacity(), 10);
+	ASSERT_ANY_THROW(q.head());
+	ASSERT_ANY_THROW(q.tail());
+}
+
+TEST(ListBasedQueueTest, SizeValueConstructor) {
+	ListBasedQueue<int> q(10, 5);
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), true);
+	EXPECT_EQ(q.size(), 10);
+	EXPECT_EQ(q.capacity(), 10);
+	EXPECT_EQ(q.head(), 5);
+	EXPECT_EQ(q.tail(), 5);
+}
+
+TEST(ListBasedQueueTest, TVectorConstructor) {
+	TVector<int> vec({ 2, 7, 45, -7 });
+	ListBasedQueue<int> q(vec);
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), true);
+	EXPECT_EQ(q.size(), 4);
+	EXPECT_EQ(q.capacity(), 4);
+	EXPECT_EQ(q.head(), 2);
+	EXPECT_EQ(q.tail(), -7);
+}
+
+TEST(ListBasedQueueTest, InitListConstructor) {
+	ListBasedQueue<int> q({ 2, 7, 45, -3 });
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), true);
+	EXPECT_EQ(q.size(), 4);
+	EXPECT_EQ(q.capacity(), 4);
+	EXPECT_EQ(q.head(), 2);
+	EXPECT_EQ(q.tail(), -3);
+}
+
+TEST(ListBasedQueueTest, CopyConstructor) {
+	ListBasedQueue<int> q1({ 9, 0, 0, 6 });
+	ListBasedQueue<int> q2(q1);
+	EXPECT_EQ(q2.isEmpty(), false);
+	EXPECT_EQ(q2.isFull(), true);
+	EXPECT_EQ(q2.size(), 4);
+	EXPECT_EQ(q2.capacity(), 4);
+	EXPECT_EQ(q2.head(), 9);
+	EXPECT_EQ(q2.tail(), 6);
+
+	q1.pop();
+	EXPECT_EQ(q2.head(), 9);
+	EXPECT_EQ(q2.size(), 4);
+	EXPECT_EQ(q2.capacity(), 4);
+}
+
+TEST(ListBasedQueueTest, AssignListBasedQueue) {
+	ListBasedQueue<int> q2({ 1, 2, 5, 4 });
+	ListBasedQueue<int> q1;
+	q1.assign(q2);
+	EXPECT_EQ(q1.isEmpty(), false);
+	EXPECT_EQ(q1.isFull(), true);
+	EXPECT_EQ(q1.size(), 4);
+	EXPECT_EQ(q1.capacity(), 4);
+	EXPECT_EQ(q1.head(), 1);
+	EXPECT_EQ(q1.tail(), 4);
+
+	q2.pop();
+	EXPECT_EQ(q1.head(), 1);
+	EXPECT_EQ(q1.tail(), 4);
+	EXPECT_EQ(q1.size(), 4);
+	EXPECT_EQ(q1.capacity(), 4);
+}
+
+TEST(ListBasedQueueTest, AssignTVector) {
+	TVector<int> data({5, 6, 3, 2, 1, 7});
+	ListBasedQueue<int> q2({ 5, 6, 3, 2, 1, 7 });
+	ListBasedQueue<int> q1;
+	q1.assign(data);
+	EXPECT_EQ(q1.isEmpty(), false);
+	EXPECT_EQ(q1.isFull(), true);
+	EXPECT_EQ(q1.size(), 6);
+	EXPECT_EQ(q1.capacity(), 6);
+	EXPECT_EQ(q1.head(), 5);
+	EXPECT_EQ(q1.tail(), 7);
+
+	data.pop_back();
+	EXPECT_EQ(q1.head(), 5);
+	EXPECT_EQ(q1.tail(), 7);
+	EXPECT_EQ(q1.size(), 6);
+	EXPECT_EQ(q1.capacity(), 6);
+}
+
+TEST(ListBasedQueueTest, ToTVector) {
+	TVector<int> data({ 1, 2, -3, 40 });
+	ListBasedQueue<int> q({ 1, 2, -3, 40 });
+	TVector<int> vec;
+	vec = q.toTVector();
+	EXPECT_TRUE(vec == data);
+}
+
+TEST(ListBasedQueueTest, PushFromFullExeption) {
+	ListBasedQueue<int> q({ 9, 0, 0, 6 });
+	ASSERT_ANY_THROW(q.push(7));
+}
+
+TEST(ListBasedQueueTest, PushFromEmpty) {
+	ListBasedQueue<int> q({ 2, 4 });
+	q.pop();
+	q.pop();
+	EXPECT_EQ(q.isEmpty(), true);
+	q.push(10);
+	EXPECT_EQ(q.isEmpty(), false);
+}
+
+TEST(ListBasedQueueTest, Push) {
+	ListBasedQueue<int> q(10);
+	q.push(-12);
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), false);
+}
+
+TEST(ListBasedQueueTest, PopFromEmpty) {
+	ListBasedQueue<int> q;
+	ASSERT_ANY_THROW(q.pop());
+}
+
+TEST(ListBasedQueueTest, Pop) {
+	ListBasedQueue<int> q({ 4, 7, -13 });
+	q.pop();
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), false);
+	EXPECT_EQ(q.tail(), -13);
+	EXPECT_EQ(q.head(), 7);
+}
+
+TEST(ListBasedQueueTest, Clear) {
+	ListBasedQueue<int> q({ 176, -45, 8 });
+	q.clear();
+	EXPECT_EQ(q.isEmpty(), true);
+	EXPECT_EQ(q.isFull(), false);
+}
+
+TEST(ListBasedQueueTest, ReserveLessElements) {
+	ListBasedQueue<int> q({ 4, 7, 8 });
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), true);
+	EXPECT_EQ(q.tail(), 8);
+	EXPECT_EQ(q.head(), 4);
+	EXPECT_EQ(q.size(), 3);
+	EXPECT_EQ(q.capacity(), 3);
+
+	q.reserve(1);
+
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), true);
+	EXPECT_EQ(q.tail(), 8);
+	EXPECT_EQ(q.head(), 4);
+	EXPECT_EQ(q.size(), 3);
+	EXPECT_EQ(q.capacity(), 3);
+}
+
+TEST(ListBasedQueueTest, ReserveMoreElements) {
+	ListBasedQueue<int> q({ 1, 2, 3 });
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), true);
+	EXPECT_EQ(q.tail(), 3);
+	EXPECT_EQ(q.head(), 1);
+	EXPECT_EQ(q.size(), 3);
+	EXPECT_EQ(q.capacity(), 3);
+
+	q.reserve(10);
+
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), false);
+	EXPECT_EQ(q.tail(), 3);
+	EXPECT_EQ(q.head(), 1);
+	EXPECT_EQ(q.size(), 3);
+	EXPECT_EQ(q.capacity(), 10);
+}
+
+TEST(ListBasedQueueTest, ShrinkToFitToEmptyException) {
+	ListBasedQueue<int> q;
+	ASSERT_ANY_THROW(q.shrinkToFit());
+}
+
+TEST(ListBasedQueueTest, ShrinkToFitFull) {
+	ListBasedQueue<int> q({ 4, 7, 8 });
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), true);
+	EXPECT_EQ(q.tail(), 8);
+	EXPECT_EQ(q.head(), 4);
+	EXPECT_EQ(q.size(), 3);
+	EXPECT_EQ(q.capacity(), 3);
+
+	q.shrinkToFit();
+
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), true);
+	EXPECT_EQ(q.tail(), 8);
+	EXPECT_EQ(q.head(), 4);
+	EXPECT_EQ(q.size(), 3);
+	EXPECT_EQ(q.capacity(), 3);
+}
+
+TEST(ListBasedQueueTest, ShrinkToFit) {
+	ListBasedQueue<int> q({ 4, 7, 8 });
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), true);
+	EXPECT_EQ(q.tail(), 8);
+	EXPECT_EQ(q.head(), 4);
+	EXPECT_EQ(q.size(), 3);
+	EXPECT_EQ(q.capacity(), 3);
+
+	q.pop();
+	q.pop();
+	q.shrinkToFit();
+
+	EXPECT_EQ(q.isEmpty(), false);
+	EXPECT_EQ(q.isFull(), true);
+	EXPECT_EQ(q.tail(), 8);
+	EXPECT_EQ(q.head(), 8);
+	EXPECT_EQ(q.size(), 1);
+	EXPECT_EQ(q.capacity(), 1);
+}
+
+TEST(ListBasedQueueTest, IsEmpty) {
+	ListBasedQueue<int> q;
+	EXPECT_EQ(q.isEmpty(), true);
+}
+
+TEST(ListBasedQueueTest, IsNotEmpty) {
+	ListBasedQueue<int> q({ 13, 51 });
+	EXPECT_EQ(q.isEmpty(), false);
+	q.pop();
+	EXPECT_EQ(q.isEmpty(), false);
+	q.pop();
+	EXPECT_EQ(q.isEmpty(), true);
+}
+
+TEST(ListBasedQueueTest, IsFull) {
+	ListBasedQueue<int> q({ -94, 13 });
+	EXPECT_EQ(q.isFull(), true);
+	q.pop();
+	EXPECT_EQ(q.isFull(), false);
+	q.pop();
+	EXPECT_EQ(q.isFull(), false);
+}
+
+TEST(ListBasedQueueTest, IsNotFull) {
+	ListBasedQueue<int> q({ 6, 66, 777 });
+	EXPECT_EQ(q.isFull(), true);
+	q.pop();
+	EXPECT_EQ(q.isFull(), false);
+	q.pop();
+	EXPECT_EQ(q.isFull(), false);
+}
