333a39401cf40e4637cbb74d28a4ec8d7aa56ae1
Артём Денисов <temadenwin@mail.ru>
2026-05-09 11:50:54 +0300

    ITable minor tweaks.

--- interface_itable/ITable.h
+++ interface_itable/ITable.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <optional>
+#include <iostream>
 
 template <class TKey, class TValue> class ITable {
 public:
@@ -8,13 +8,13 @@ public:
 
     virtual void insert(const TKey&, const TValue&) = 0;
     virtual void erase(const TKey&) = 0;
+
     virtual TValue* find(const TKey&) noexcept = 0;
+
     virtual bool isEmpty() const noexcept = 0;
-
-    virtual void print(std::ostream& out) const = 0;
-
     virtual bool contains(const TKey&) const noexcept = 0;
 
+    virtual void print(std::ostream& out) const = 0;
     friend std::ostream& operator<<(std::ostream& out, const ITable& table) {
         table.print(out);
         return out;
