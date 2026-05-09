5c1132e09ae2973b84711c1ed72d3b8a457cbb1e
Артём Денисов <temadenwin@mail.ru>
2026-05-09 11:51:49 +0300

    Implemented class Unsorted Table On Array and covered it with tests.

--- table_unsorted_table_on_array/UnsortedOnArray.h
+++ table_unsorted_table_on_array/UnsortedOnArray.h
@@ -3,8 +3,73 @@
 #include "../interface_itable/ITable.h"
 #include "../lib_TVector/TVector.h"
 
-template <class TKey, class TValue> class UnsortedTableOnArray : public ITable<TKey, TValue> {
-    TVector<std::pair<TKey, TVal>> _rows;
+template<class TKey, class TValue> class UnsortedTableOnArray : public ITable<TKey, TValue> {
+    TVector<std::pair<TKey, TValue>> _rows;
 public:
+    UnsortedTableOnArray() = default;
 
+    void insert(const TKey&, const TValue&) override;
+    void erase(const TKey&) override;
+
+    TValue* find(const TKey&) noexcept override;
+
+    inline bool isEmpty() const noexcept override { return _rows.is_empty(); };
+    bool contains(const TKey&) const noexcept override;
+
+    void print(std::ostream&) const override;
+private:
+    int keyExistsPos(const TKey&) const noexcept;
 };
+
+template<class TKey, class TValue> void UnsortedTableOnArray<TKey, TValue>::insert(const TKey& key, const TValue& value) {
+    int pos = keyExistsPos(key);
+    if (pos == -1) {
+        _rows.push_back({ key, value });
+    }
+    else {
+        _rows[pos].second = value;
+    }
+}
+
+template<class TKey, class TValue> void UnsortedTableOnArray<TKey, TValue>::erase(const TKey& key) {
+    int pos = keyExistsPos(key);
+    if (pos != -1) {
+        _rows.erase(pos);
+    }
+}
+
+template<class TKey, class TValue> TValue* UnsortedTableOnArray<TKey, TValue>::find(const TKey& key) noexcept {
+    int pos = keyExistsPos(key);
+    if (pos != -1) {
+        return &_rows[pos].second;
+    }
+    else {
+        return nullptr;
+    }
+}
+
+template<class TKey, class TValue> bool UnsortedTableOnArray<TKey, TValue>::contains(const TKey& key) const noexcept {
+    int pos = keyExistsPos(key);
+    if (pos != -1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+template<class TKey, class TValue> void UnsortedTableOnArray<TKey, TValue>::print(std::ostream& out) const {
+    out << "UnsortedTableOnArray with " << _rows.size() << " rows:" << std::endl;
+    for (size_t i = 0; i < _rows.size(); i++) {
+        out << " " << i << " " << _rows[i].first << " : " << _rows[i].second << std::endl;
+    }
+}
+
+template<class TKey, class TValue> int UnsortedTableOnArray<TKey, TValue>::keyExistsPos(const TKey& key) const noexcept {
+    for (size_t i = 0; i < _rows.size(); i++) {
+        if (_rows[i].first == key) {
+            return i;
+        }
+    }
+    return -1;
+}
--- tests/test_UnsortedTableOnArr.cpp
+++ tests/test_UnsortedTableOnArr.cpp
@@ -1,1 +1,100 @@
 #include <gtest/gtest.h>
+#include "../table_unsorted_table_on_array/UnsortedOnArray.h"
+
+TEST(UnsortedTableOnArrayTest, InsertAndFind) {
+    UnsortedTableOnArray<int, std::string> table;
+    table.insert(10, "ten");
+    table.insert(20, "twenty");
+    table.insert(5, "five");
+
+    auto* val = table.find(20);
+    ASSERT_NE(val, nullptr);
+    EXPECT_EQ(*val, "twenty");
+
+    val = table.find(5);
+    ASSERT_NE(val, nullptr);
+    EXPECT_EQ(*val, "five");
+
+    EXPECT_EQ(table.find(42), nullptr);
+}
+
+TEST(UnsortedTableOnArrayTest, UpdateValue) {
+    UnsortedTableOnArray<std::string, int> table;
+    table.insert("apple", 100);
+    table.insert("banana", 200);
+
+    table.insert("apple", 999);
+
+    auto* val = table.find("apple");
+    ASSERT_NE(val, nullptr);
+    EXPECT_EQ(*val, 999);
+    val = table.find("banana");
+    EXPECT_EQ(*val, 200);
+}
+
+TEST(UnsortedTableOnArrayTest, EraseKey) {
+    UnsortedTableOnArray<char, double> table;
+    table.insert('A', 1.5);
+    table.insert('B', 2.5);
+    table.insert('C', 3.5);
+
+    table.erase('B');
+    EXPECT_EQ(table.find('B'), nullptr);
+    EXPECT_NE(table.find('A'), nullptr);
+    EXPECT_NE(table.find('C'), nullptr);
+
+    table.erase('Z');
+
+    table.erase('A');
+    table.erase('C');
+    EXPECT_TRUE(table.isEmpty());
+}
+
+TEST(UnsortedTableOnArrayTest, CheckEmpty) {
+    UnsortedTableOnArray<int, bool> table;
+    EXPECT_TRUE(table.isEmpty());
+
+    table.insert(1, true);
+    EXPECT_FALSE(table.isEmpty());
+
+    table.erase(1);
+    EXPECT_TRUE(table.isEmpty());
+}
+
+TEST(UnsortedTableOnArrayTest, TestPointer) {
+    UnsortedTableOnArray<int, int> table;
+    table.insert(42, 100);
+
+    int* ptr = table.find(42);
+    ASSERT_NE(ptr, nullptr);
+    *ptr = 500;
+
+    auto* check = table.find(42);
+    EXPECT_EQ(*check, 500);
+}
+
+TEST(UnsortedTableOnArrayTest, ContainsKey) {
+    UnsortedTableOnArray<double, std::string> table;
+    table.insert(3.14, "pi");
+    table.insert(2.71, "e");
+
+    EXPECT_TRUE(table.contains(3.14));
+    EXPECT_TRUE(table.contains(2.71));
+    EXPECT_FALSE(table.contains(1.0));
+    EXPECT_FALSE(table.contains(0.0));
+}
+
+TEST(UnsortedTableOnArrayTest, InsertAfterErase) {
+    UnsortedTableOnArray<int, int> table;
+    table.insert(1, 1);
+    table.insert(2, 2);
+    table.insert(3, 3);
+    table.erase(2);
+    table.insert(4, 4);
+
+    EXPECT_TRUE(table.contains(1));
+    EXPECT_FALSE(table.contains(2));
+    EXPECT_TRUE(table.contains(3));
+    EXPECT_TRUE(table.contains(4));
+    EXPECT_EQ(*table.find(4), 4);
+}
